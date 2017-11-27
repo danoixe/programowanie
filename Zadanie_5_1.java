73d3893c6c3f76e927562c3760b809167c99117d
danoixe <33001136+danoixe@users.noreply.github.com>
2017-11-27 17:58:55 +0100

    Add files via upload

---
+++ Zadanie_5_1.java
@@ -0,0 +1,27 @@
+
+public class Zadanie_5_1 {
+
+	public static void main(String[] args) {
+		int n=4;
+		int j=1;
+		
+		// iteracyjnie
+		for (int i=1; i<=n; i++) {
+			j=j*i;
+		}
+		
+		System.out.println(j);
+		
+		
+		// rekurencyjnie
+		for ()
+
+	}
+	
+	public static int silnia(int m) {
+		
+		
+		return m;
+	}
+
+}
